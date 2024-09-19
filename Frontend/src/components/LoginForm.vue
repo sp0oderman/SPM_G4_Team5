cdade150e607e6f630eaea4c74502a94dca89098
Alastair Lim <alastair0852@gmail.com>
2024-09-19 22:30:54 +0800

    Corrected prior env file naming issue

--- Frontend/src/components/LoginForm.vue
+++ Frontend/src/components/LoginForm.vue
@@ -68,3 +68,86 @@
   }
   </style>
   
+<!-- Sample Login Logic -->
+  <!-- <script>
+  import { validationMixin } from "vuelidate";
+  import { required } from "vuelidate/lib/validators";
+  
+  import { useUserStore } from "@/store/user";
+  import { mapActions } from 'pinia'
+  import { mapState } from 'pinia'
+  
+  //const store = useUserStore()
+  const loginUrl = "https://personal-swk23gov.outsystemscloud.com/User_API/rest/v1/user/login/";
+  
+  export default {
+    mixins: [validationMixin],
+    data() {
+      return {
+          email: "",
+          password: "",
+          valid: null,
+      };
+    },
+    validations: {
+      email: {
+        required,
+      },
+      password: {
+        required,
+      },
+    },
+    computed:{
+      ...mapState(useUserStore, ['getUserId']),
+    },
+    methods: {
+      ...mapActions(useUserStore, ['setUserId']),
+      login(){
+        console.log("Before login userId: " + this.getUserId);
+        const loginDetails = {
+            email: this.email,
+            password: this.password
+        }
+        fetch(loginUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginDetails),
+        })
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response error');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if(data.Result.success){
+                this.valid = true;
+                // this.usedId = data.AuctionUser.userId;
+                console.log(data.AuctionUser.userId);
+                this.setUserId(data.AuctionUser.userId);
+                console.log("getUserId is " + this.getUserId);
+                this.$router.push({ name: 'Listings' });
+            }
+            else{
+                console.log("Wrong email / password");
+                console.log("getUserId is " + this.getUserId);
+                this.valid = false;
+            }
+        })
+        .catch(error => {
+            console.error('Error logging in:', error);
+        });
+      },
+      getValidationClass(fieldName) {
+        const field = this.$v[fieldName];
+        if (field) {
+          return {
+            "md-invalid": field.$invalid && field.$dirty,
+          };
+        }
+      },
+    },
+  };
+  </script> -->
