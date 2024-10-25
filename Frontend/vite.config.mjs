e40f110f542d4fbbf46453370e86ca50c08ca6d4
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-10-25 15:49:27 +0800

    Deployment (#36)
    * adjusted structure by moving __init__ into Backend, added vercel.json, ran npm run build, altere requirements.txt
    
    * Added more functions to email_functions.py
    
    * adjusted configs in vite
    
    * homepage of app added
    
    ---------
    
    Co-authored-by: sp0oderman <88531899+sp0oderman@users.noreply.github.com>

--- Frontend/vite.config.mjs
+++ Frontend/vite.config.mjs
@@ -13,6 +13,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   plugins: [
     VueRouter(),
     Layouts(),
