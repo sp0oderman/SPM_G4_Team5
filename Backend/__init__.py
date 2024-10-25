93a5cd194e609dfc4d1c5863d083af42a9636a1b
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-10-25 16:28:06 +0800

    added logging to see if connected to db

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Initialize extensions
 db = SQLAlchemy()
 session = Session()
@@ -13,8 +17,19 @@ def create_app(config_class):
     # Load configuration
     app.config.from_object(config_class)
     
+    #---------------------------------------------
+    # Retrieve the database URL from the environment
+    database_url = os.getenv("DATABASE_URL")
+    print("DATABASE_URL:", database_url)  # Print the database URL
+
+    if not database_url:
+        print("Error: No DATABASE_URL environment variable set")
+        raise ValueError("No DATABASE_URL environment variable set")
+    #---------------------------------------------
+
     # Initialize extensions
     db.init_app(app)
+    print("Database initialized")  # Confirm database initialization
     session.init_app(app)
 
     from src.services.employees_services import Employees_Service
