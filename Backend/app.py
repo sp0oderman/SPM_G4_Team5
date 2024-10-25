7364f3d0026e2fb088ebb7320cddd19746d08d43
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-10-25 15:45:06 +0800

    homepage of app added

--- Backend/app.py
+++ Backend/app.py
@@ -5,5 +5,9 @@ from flask_cors import CORS
 app = create_app(config_class=DevelopmentConfig)
 CORS(app)
 
+@app.route('/api/homepage')
+def homepage():
+    return {"message": "Homepage of flask app!"}
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
