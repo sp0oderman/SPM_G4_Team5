c5c058eb3ffdcf2eaff78c6adb09f0f32b551638
JakobLie <142589068+JakobLie@users.noreply.github.com>
2024-11-01 11:25:51 +0800

    Adjusted Email Functions (#50)
    * added invokes.py wrapper to invoke microservices via URL
    
    * created function to insert wfh_request in dedicated db MS, additionally created the complex MS to handle the applciation for wfh_request
    
    * adjusted function to insert wfh_application record in employee.py
    
    * added function to wfh_request.py that allows user to delete recorded wfh_requests using request_id
    
    * Test branch function
    
    * test test, this is a test
    
    * test test, this is a test 222222
    
    * test 4
    
    * test 5
    
    * edited wfh_request function to get team_requests by reporting manager AND department
    
    * Added same function to find_by team according to reporting_manager and dept to employee.py
    
    * Adjusted get_by_team functions to only search by reporting_manager ID, edited setup.sql to sync with current DB design, added small frontend html file to test Microservices
    
    * Updated directory again
    
    * Pushing Jakob's changes
    
    * changed backend homepage and frontend login page, to check CD
    
    * Adjusted tests, added email_functions in util, implemented them in the wfh_request_services, checked integration tests before committing
    
    * made get_all route be able to filter by status
    
    * adjusted automated tests to reduce minutes used
    
    * fixed tests due to previous route alteration
    
    * Added folder for unit tests and adjusted ci.yml
    
    * adjusted the path added to sys in tests
    
    * test ci
    
    * test ci
    
    * adjusted functionality of get_by_team wfh requests, to get all subordinates even under manager who report to input staff
    
    * break workflow
    
    * Adjusted Email functions with new API, adjusted methods from POST to PUT for reject/approve
    
    ---------
    
    Co-authored-by: sp0oderman <88531899+sp0oderman@users.noreply.github.com>

--- Frontend/src/components/hr/HRMenuBar.vue
+++ Frontend/src/components/hr/HRMenuBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>SPM WFH Project</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn text @click="overallSchedule">Overall Schedule</v-btn>
-      <v-btn text @click="logout">Logout</v-btn>
-    </v-app-bar>
-  </template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>SPM WFH Project</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text @click="overallSchedule">Overall Schedule</v-btn>
+    <v-btn text @click="logout">Logout</v-btn>
+  </v-app-bar>
+</template>
 
 <script>
 import { useAuthStore } from '@/stores/auth';
@@ -13,7 +13,7 @@ import { useAuthStore } from '@/stores/auth';
 export default {
   methods: {
     overallSchedule(){
-      this.$router.push({ name: '/HR/OverallSchedule' });
+      this.$router.push({ name: '/hr/OverallSchedule' });
     },
     logout() {
       const authStore = useAuthStore();
